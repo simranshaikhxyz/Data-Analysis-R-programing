04b0f8f5c9e7713f2bb0ad9be6bfb1642cef035d
Simran Shaikh <simranshaikhxyz@gmail.com>
2025-11-24 19:15:37 +0530

    Delete 1st.R

--- 1st.R
+++
@@ -1,8 +0,0 @@
-tips <- read.csv("C:/Users/mvluc/Downloads/simranpython/tips.csv", stringsAsFactors=TRUE)
-View(tips)
-lm(tip~size)
-attach(tips)
-
-temp <- read.csv("~/temp.txt", stringsAsFactors=TRUE)
-lm(ID ~ Pressure)
-attach(temp)
