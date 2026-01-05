b8f01cf2cdb69bfe89ab6311c055ed666ed143f3
Simran Shaikh <simranshaikhxyz@gmail.com>
2026-01-05 12:33:27 +0530

    2nd.R

--- 2nd.R
+++ 2nd.R
@@ -4,10 +4,6 @@ data2 <- read.csv("Amazon.csv")
 
 head(data2, 10)
 
-# -----------------------------------
-# Frequency tables using table()
-# -----------------------------------
-
 # Frequency of Category
 table(data2$Category)
 
@@ -17,11 +13,8 @@ table(data2$Brand)
 # Frequency of PaymentMethod
 table(data2$PaymentMethod)
 
-# -----------------------------------
-# Frequency tables using count()
-# -----------------------------------
-
 count(data2, Category)
 count(data2, Brand)
 count(data2, PaymentMethod)
 print("simran s113")
+
