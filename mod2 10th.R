bbf5c2ed70e45eea30fe31c96c2fd8d0121f1ddd
Simran Shaikh <simranshaikhxyz@gmail.com>
2026-01-13 19:26:01 +0530

    mod2 10th.R

--- mod2 10th.R
+++ mod2 10th.R
@@ -32,3 +32,33 @@ ggplot(mydata, aes(x = pack_size, y = price_inr, color = brand_name)) +
        x = "Pack Size",
        y = "Price (INR)") +
   theme_minimal()
+
+# pie chart
+ggplot(mydata, aes(x = "", fill = dosage_form)) +
+  geom_bar(width = 1) +
+  coord_polar("y") +
+  labs(
+    title = "Pie Chart of Products by Dosage Form",
+    fill = "Dosage Form"
+  ) +
+  theme_void()
+
+# high low chart
+ggplot(mydata, aes(x = brand_name)) +
+  geom_linerange(
+    aes(
+      ymin = price_inr - 10,
+      ymax = price_inr + 10
+    ),
+    color = "darkgreen",
+    size = 1
+  ) +
+  geom_point(aes(y = price_inr), color = "red", size = 2) +
+  labs(
+    title = "High–Low Chart of Product Prices by Brand",
+    x = "Brand",
+    y = "Price (INR)"
+  ) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
